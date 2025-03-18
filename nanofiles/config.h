18faf611b40fadc67601f8ec2bc4704baff38bd2
arf20 <aruizfernandez05@gmail.com>
2025-03-18 01:48:51 +0100

    WIP main nanofiles

--- nanofiles/config.h
+++ nanofiles/config.h
@@ -1,13 +1,14 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define NF_PROTOCOL_ID  "20032005"
+#define DEFAULT_SHARED_DIRNAME      "nf-shared"
 
-#define MAX_DGRAM_SIZE  65507
+#define DEFAULT_DIRECTORY_HOSTNAME  "localhost"
 
-#define MAX_FORWARDINGS 5
-
-#define SOCK_TIMEOUT    1  /* seconds */
+#define NF_PROTOCOL_ID              "20032005"
+#define MAX_DGRAM_SIZE              65507
+#define MAX_FORWARDINGS             5
+#define SOCK_TIMEOUT                1  /* seconds */
 
 #endif /* _CONFIG_H */
 
--- nanofiles/main.c
+++ nanofiles/main.c
@@ -1,10 +1,38 @@
+#include "config.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "directory_connector.h"
 
-int
-main()
+void
+usage(const char *argv0)
 {
+    fprintf(stderr,
+        "usage: %s [-d <shared directory>] [-s <directory hostname>]\n",
+        argv0);
+    exit(1);
+}
+
+int
+main(int argc, char **argv)
+{
+    const char *directory_hostname = DEFAULT_DIRECTORY_HOSTNAME;
+    const char *shared_dirname = DEFAULT_SHARED_DIRNAME;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] != '-' || i + 1 >= argc)
+            usage(argv[0]);
+
+        if (argv[i][1] == 'd') {
+            directory_hostname = argv[i + 1];
+            i++;
+        } else if (argv[i][1] == 's') {
+            shared_dirname = argv[i + 1];
+            i++;
+        } else
+            usage(argv[0]);
+    }
 
 }
 
